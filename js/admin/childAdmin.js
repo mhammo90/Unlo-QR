d0559f4841903a92510c1b690bef9c0436e4ca26
Mike Hammersley <Mike.Hammersley90@gmail.com>
2023-08-08 22:54:18 +0100

    importChild and updateChild - async

--- js/admin/childAdmin.js
+++ js/admin/childAdmin.js
@@ -1,33 +1,53 @@
-// IMPORT CLASS
+// REQUIRES //
+const fs = require("fs").promises;
+const path = require("path");
+
+// IMPORT CLASS //
 const Child = require(relPath("./js/classes/Child"));
-// DATA LOCATION
+// DATA LOCATION //
 const childDataLoc = relPath(process.env.CHILD_DATA) || relPath("./data/children");
 
-function importChild(cName) {
+async function importChild(cName) {
 	var name = cName.charAt(0).toUpperCase() + cName.slice(1);
-	var child = require(`${childDataLoc}/${name}.json`);
-	var childObj = new Child(
-		child.name,
-		child.ip,
-		child.refreshInterval,
-		child.maxPoints,
-		child.status,
-		child.currentPoints,
-		child.blockTime,
-		child.unblockTime
-	);
-	return childObj;
+	var filePath = path.join(childDataLoc, `${name}.json`);
+	try {
+		const data = await fs.readFile(filePath, "utf-8");
+		const child = JSON.parse(data);
+		return new Child(
+			child.name,
+			child.ip,
+			child.refreshInterval,
+			child.maxPoints,
+			child.status,
+			child.currentPoints,
+			child.blockTime,
+			child.unblockTime
+		);
+	} catch (error) {
+		console.error(`Error Importing ${name}: ${error}`);
+		return null;
+	}
 }
 
-function updateChild(cName, key, value) {
+async function updateChild(cName, key, value) {
 	var name = cName.charAt(0).toUpperCase() + cName.slice(1);
-	var child = importChild(name);
-	if (child.key === value) {
+	var filePath = path.join(childDataLoc, `${name}.json`);
+	var child = await importChild(name);
+	if (!child) {
+		console.error(`Child ${name} not found.`);
+		return;
+	}
+	if (child[key] === value) {
 		return;
 	} else {
 		child.update(key, value);
 		var output = JSON.stringify(child, null, 2);
-		fs.writeFileSync(`${childDataLoc}/${name}.json`, output);
+		try {
+			await fs.writeFile(filePath, output, "utf-8");
+			console.log(`${name} updated successfully`);
+		} catch (error) {
+			console.error(`Error updating ${name}: ${error}`);
+		}
 	}
 }
 
