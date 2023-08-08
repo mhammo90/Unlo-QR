27552ee61650e9e26273b2d664fb3763ffc7c73c
Mike Hammersley <Mike.Hammersley90@gmail.com>
2023-08-09 00:56:48 +0100

    getTaskIDs, getTaskNames, getAllTasks, showTaskQR

--- js/tasks/tasks.js
+++ js/tasks/tasks.js
@@ -1,49 +1,52 @@
 // REQUIRES AND IMPORTS //
 const fs = require("fs");
-const { taskDataLoc, Task, importTask } = require(relPath("./js/admin/tasksAdmin"));
+const { taskDataLoc, importTask } = require(relPath("./js/admin/tasksAdmin"));
 
 // GET A LIST OF ALL THE TASK IDS - returns ARRAY //
-function getTaskIDs() {
-	const taskIds = [];
-	const files = fs.readdirSync(taskDataLoc);
-	files.forEach((file) => {
-		var id = file.slice(0, -5);
-		taskIds.push(id);
-	});
-	return taskIds;
+async function getTaskIDs() {
+	try {
+		const tasks = await fs.promises.readdir(taskDataLoc);
+		return tasks.map((task) => task.slice(0, -5));
+	} catch (error) {
+		console.error(`An Error Occured: ${error}`);
+	}
 }
 
 // GET ALL TASK NAMES - returns ARRAY //
-function getTaskNames() {
-	const taskNames = [];
-	const ids = getTaskIDs();
-	ids.forEach((id) => {
-		var task = importTask(id);
-		taskNames.push(task);
-	});
+async function getTaskNames() {
+	var names = [];
+	try {
+		const ids = await getTaskIDs();
+		for (const id in ids) {
+			var task = await importTask(id);
+			names.push(task.taskName);
+		}
+		return names;
+	} catch (error) {
+		console.error(`An Error Occured: ${error}`);
+	}
 }
 
 // GET ALL TASKS AS OBJECTS AND RETURN THEM AS AN ARRAY - returns ARRAY//
-function getAllTasks() {
-	tasksArr = [];
-	var tasks = getTaskIDs();
-	tasks.forEach((task) => {
-		var info = importTask(task);
-		tasksArr.push(info);
-	});
-	return tasksArr;
-}
-
-// GET POINTS OF TASK - returns INTEGER //
-function getTaskPoints(taskID) {
-	return importTask(taskID).taskPoints;
+async function getAllTasks() {
+	try {
+		var ids = await getTaskIDs();
+		var tasks = ids.map(async (id) => {
+			var task = await importTask(id);
+			return task;
+		});
+		return tasks;
+	} catch (error) {
+		console.error(`An Error Occured: ${error}`);
+	}
 }
 
 // SHOW TASK QR CODE - returns QRCODE base64 STRING //
-function showTaskQR(taskID) {
-	const task = importTask(taskID);
+async function showTaskQR(taskID) {
+	const task = await importTask(taskID);
+
 	return task.showQR();
 }
 
 // EXPORT getTaskIDs(), getTask(), getAllTasks() //
-module.exports = { getTaskIDs, getTaskNames, getAllTasks, getTaskPoints, showTaskQR };
+module.exports = { getTaskIDs, getTaskNames, getAllTasks, showTaskQR };
