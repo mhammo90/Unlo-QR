eb188c4460e25c5cb79ffe6b0c6f00cfe1b0e2d4
Mike Hammersley <Mike.Hammersley90@gmail.com>
2023-08-08 19:26:48 +0100

    "/task" route added to adminApp

--- routes/admin/taskRoutes.js
+++ routes/admin/taskRoutes.js
@@ -5,20 +5,20 @@ const { getAllTasks, showTaskQR } = require(relPath("./js/tasks/tasks.js"));
 const { importTask } = require(relPath("./js/admin/tasksAdmin"));
 
 // RETURN ALL TASKS (JSON) //
-router.get("/task", (req, res) => {
+router.get("/", (req, res) => {
 	const tasks = getAllTasks();
 	res.json(tasks);
 });
 
 // RETURN SINGLE TASK (JSON) //
-router.get("/task/:id", (req, res) => {
+router.get("/:id", (req, res) => {
 	var id = req.params.id;
 	const task = importTask(id);
 	res.json(task);
 });
 
 // RETURN QR WITH <IMG> TAGS //
-router.get("/task/:id/qr", (req, res) => {
+router.get("/:id/qr", (req, res) => {
 	var id = req.params.id;
 	const qr = showTaskQR(id);
 	res.send(`<img src="${qr}"></img>`);
