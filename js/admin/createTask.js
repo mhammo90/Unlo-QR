16b827a8dec2d22b824c712ae3eb93fb8e300a63
Mike Hammersley <Mike.Hammersley90@gmail.com>
2023-08-09 00:53:53 +0100

    createTask async

--- js/admin/createTask.js
+++ js/admin/createTask.js
@@ -1,14 +1,20 @@
-// REQUIRES and IMPORTS
-const fs = require("fs");
+// REQUIRES and IMPORTS //
+const fs = require("fs").promises;
+const path = require("path");
 const { Task, taskDataLoc } = require(relPath("./js/admin/tasksAdmin"));
 
-// CREATE TASK
+// CREATE TASK (ASYNC) //
 async function createTask(name, points) {
 	var newTask = new Task(undefined, name, points);
 	await newTask.createQR();
 	var output = JSON.stringify(newTask, null, 2);
-	fs.writeFileSync(`${taskDataLoc}/${newTask.id}.json`, output);
+	var filePath = path.join(taskDataLoc, `${newTask.id}.json`);
+	try {
+		fs.writeFile(filePath, output, "utf-8");
+		console.log(`${newTask.name} created succesfully with ${newTask.id}`);
+	} catch (error) {
+		console.error(`Error creating Task: ${error}`);
+	}
 }
 
-// EXPORT createTask()
 module.exports = createTask;
