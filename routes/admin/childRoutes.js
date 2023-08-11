3aeba1de9cb44a1c683308c87f5f205f0ecdb3ae
Mike Hammersley <Mike.Hammersley90@gmail.com>
2023-08-11 13:34:34 +0100

    change child routes /update + /status (overide)

--- routes/admin/childRoutes.js
+++ routes/admin/childRoutes.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const { importChild, updateChild } = require(relPath("./js/admin/childAdmin"));
-const { getAllChildren } = require(relPath("./js/children/helper"));
+const { getAllChildren, getStatus } = require(relPath("./js/children/helper"));
+const { unblockChild, blockChild } = require(relPath("./js/admin/gatewayAdmin"));
 
 // RETURN ALL CHILDREN (JSON) //
 router.get("/", async (req, res) => {
@@ -18,12 +19,28 @@ router.get("/:name", async (req, res) => {
 });
 
 // UPDATE KEY = VALUE PAIRS FOR CHILD //
-router.post("/:name/:key/:value", async (req, res) => {
+router.post("/:name/update/:key/:value", async (req, res) => {
 	var name = req.params.name;
 	var key = req.params.key;
 	var value = req.params.value;
 	await updateChild(name, key, value);
 	res.send(`${name} : ${key} updated to ${value}`);
+});
+
+// MANUAL OVERIDE FOR TOGGLE BLOCK STATUS //
+// 0 - unblock //
+// 1 - block //
+router.post("/:name/status/:status", async (req, res) => {
+	var name = req.params.name;
+	var status = req.params.status;
+	var currStat = getStatus(name);
+	if ((status === 1 || status === "block") && currStat === "unblocked") {
+		blockChild(name);
+	} else if ((status === 0 || status === "unblock") && currStat === "blocked") {
+		unblockChild(name);
+	} else {
+		res.send("0 / unblock || 1 / block");
+	}
 });
 
 module.exports = router;
